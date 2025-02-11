1a911edcf9573c7590709fc4d20d50e82f4ce0c5
doirep_rodnoc <51051070+doirep-rodnoc@users.noreply.github.com>
2025-02-12 00:52:21 +0900

    register修正

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -9,20 +9,23 @@ import {
 import { IoLogoGoogle } from "react-icons/io";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useUserData } from "../../hooks/useUserData";
+import Loading from "../loading/Loading";
 
 export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [user, loading, error] = useAuthState(auth);
+  //const [user, loading, error] = useAuthState(auth);
   const nav = useNavigate();
   // const { user, isFetching, error, dispatch } = useContext(AuthContext);
+  const { userData, isLoading, isError, mutate } = useUserData();
 
   useEffect(() => {
-    if (loading) return;
-    if (user) nav("/home");
-  }, [user, loading]);
+    if (isLoading) return;
+    if (userData) nav("/home");
+  }, [userData, isLoading]);
 
   const validateEmail = (email) => {
     // 簡易的なメールアドレスの正規表現
@@ -45,57 +48,61 @@ export default function Register() {
 
   return (
     <>
-      <div className={styles.login}>
-        <div className={styles.loginWrapper}>
-          <form className={styles.loginForm} onSubmit={handleRegister}>
-            <h3>新規登録</h3>
-            <div className={styles.inputItem}>
-              <label htmlFor="email">ユーザ名:</label>
-              <input
-                type="name"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputItem}>
-              <label htmlFor="email">メールアドレス:</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className={styles.inputItem}>
-              <label htmlFor="password">パスワード:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <button type="submit" className={styles.loginButton}>
-                登録
-              </button>
-            </div>
-            {errorMessage && <p className={styles.error}>{errorMessage}</p>}
-            <div className={styles.signup}>
-              <button
-                className={styles.signupButton}
-                onClick={signUpWithGoogle}
-              >
-                Googleでサインアップ
-              </button>
-            </div>
-          </form>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className={styles.login}>
+          <div className={styles.loginWrapper}>
+            <form className={styles.loginForm} onSubmit={handleRegister}>
+              <h3>新規登録</h3>
+              <div className={styles.inputItem}>
+                <label htmlFor="email">ユーザ名:</label>
+                <input
+                  type="name"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.inputItem}>
+                <label htmlFor="email">メールアドレス:</label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.inputItem}>
+                <label htmlFor="password">パスワード:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <button type="submit" className={styles.loginButton}>
+                  登録
+                </button>
+              </div>
+              {errorMessage && <p className={styles.error}>{errorMessage}</p>}
+              <div className={styles.signup}>
+                <button
+                  className={styles.signupButton}
+                  onClick={signUpWithGoogle}
+                >
+                  Googleでサインアップ
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
